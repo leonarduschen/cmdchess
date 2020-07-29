9f7fac53b476b9870e7f262bae8b1217cd6e06fe
simple-spherical-cow <leonardus.chen@gmail.com>
2020-07-29 19:04:12 +0800

    Minor changes

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='cmdchess',
     version='0.1.0',
-    description='Command-line Chess - Play Chess on Command Line',
+    description='Command-line Application Chess',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='simple-spherical-cow',
     email = 'leonardus.chen@gmail.com',
     url='https://github.com/simple-spherical-cow/cmdchess',
