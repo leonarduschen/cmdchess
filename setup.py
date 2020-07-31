b9e4fc4e1ee4a6ca06de8127c093c1558b6a0dba
simple-spherical-cow <leonardus.chen@gmail.com>
2020-07-31 17:30:50 +0800

    Read license in setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='cmdchess',
     version='0.1.0',
@@ -10,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='simple-spherical-cow',
-    email = 'leonardus.chen@gmail.com',
+    author_email = 'leonardus.chen@gmail.com',
     url='https://github.com/simple-spherical-cow/cmdchess',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
